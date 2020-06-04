6b00b96a6ba682543afc83d3c2be8c04532e791f
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-04 16:35:31 +0530

    [Srinidhi]Added: Quantity measurment added model class

---
+++ QuantityMeasurmentCommanLayer/MeasurmentModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace QuantityMeasurmentCommanLayer
+{
+    public class MeasurmentModel
+    {
+        private double Feet;
+        private double Inch;
+        private double Centimeter;
+        private double Yard ;
+        private double Litre;
+        private double Mililitre ;
+        private double Gallon ;
+        private double KiloGram ;
+        private double Grams ;
+        private double Tonne ;
+        private double Fahrenheit ;
+        private double Celsius ;
+        public Double feet
+        {
+            set
+            {
+                this.Feet = value;
+            }
+            get
+            {
+                return this.Feet;
+            }
+        }
+        public Double inch
+        {
+            set
+            {
+                this.Inch = value;
+            }
+            get
+            {
+                return this.Inch;
+            }
+        }
+        public Double centimeter
+        {
+            set
+            {
+                this.Centimeter = value;
+            }
+            get
+            {
+                return this.Centimeter;
+            }
+        }
+        public Double yard
+        {
+            set
+            {
+                this.Yard = value;
+            }
+            get
+            {
+                return this.Yard;
+            }
+        }
+        public Double litre
+        { 
+            set
+            {
+                this.Litre = value;
+            }
+            get
+            {
+                return this.Litre;
+            }
+        }
+        public Double mililitre
+        {
+            set
+            {
+                this.Mililitre = value;
+            }
+            get
+            {
+                return this.Mililitre;
+            }
+        }
+        public Double gallon
+        {
+            set
+            {
+                this.Gallon = value;
+            }
+            get
+            {
+                return this.Gallon;
+            }
+        }
+        public Double kiloGram
+            {
+            set
+            {
+                this.KiloGram = value;
+            }
+            get
+            {
+                return this.KiloGram;
+            }
+        }
+        public Double grams
+        {
+            set
+            {
+                this.Grams = value;
+            }
+            get
+            {
+                return this.Grams;
+            }
+        }
+        public Double tonne
+        {
+            set
+            {
+                this.Tonne = value;
+            }
+            get
+            {
+                return this.Tonne;
+            }
+        }
+        public Double fahrenheit
+        {
+            set
+            {
+                this.Fahrenheit = value;
+            }
+            get
+            {
+                return this.Fahrenheit;
+            }
+        }
+    }
+}
+    
