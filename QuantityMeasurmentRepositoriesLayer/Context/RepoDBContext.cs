5220b07169f579367dfc26b92987f39c5f78d528
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-05 18:58:04 +0530

    [Srinidhi]Added: Quantity measurment added model class

---
+++ QuantityMeasurmentRepositoriesLayer/Context/RepoDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuantityMeasurmentCommanLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurmentRepositoriesLayer.Context
+{
+    public class RepoDBContext : DbContext
+    {
+        public RepoDBContext(DbContextOptions<RepoDBContext> options) : base(options)
+        {
+        }
+        public DbSet<MeasurmentModel> Measurments { get; set; }
+    }
+}
---
+++ QuantityMeasurmentRepositoriesLayer/Repository/IQuantityRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurmentRepositoriesLayer.Repository
+{
+    public interface IQuantityRepository
+    {
+
+    }
+}
