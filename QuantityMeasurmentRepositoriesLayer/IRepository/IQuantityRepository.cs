c76116290d1be2e96acb221062bd159577d71aca
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-07 14:33:48 +0530

    [Srinidhi]Added: Quantity measurment to calcluate all the quantitys

---
+++ QuantityMeasurmentRepositoriesLayer/IRepository/IQuantityRepository.cs
@@ -0,0 +1,43 @@
+﻿using QuantityMeasurmentCommanLayer;
+using QuantityMeasurmentCommanLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurmentRepositoriesLayer.Repository
+{
+    /// <summary>
+    /// Length(inch,feet,yard,centimeter)
+    /// Tempareture(fahreheit,celsius
+    /// Volume(mililiter,liter,gallon)
+    /// Weight(kg,gram,tonne)
+    /// </summary>
+    /// <param name="quantity"></param>
+    /// <returns></returns>
+    public interface IQuantityRepository
+    {     
+        ////Length
+        Object FeetToInch(Length quantity);
+        Object InchToFeet(Length quantity);
+        Object FeetToYard(Length quantity);
+        Object YardToFeet(Length quantity);
+        Object InchToCentimeter(Length quantity);
+        Object CentimeterToInch(Length quantity);
+
+        ////Tempareture
+        Object FahrenheitToCelsius(Temperature quantity);
+        Object CelsiusToFahrenheit(Temperature quantity);
+
+        ////Volume
+        Object MililiterToLiter(Volume quantity);
+        Object LiterToMiliter(Volume quantity);
+        Object LiterToGallon(Volume quantity);
+        Object GallonToLiter(Volume quantity);
+
+        ////Weight
+        Object KgToGram(Weight quantity);
+        Object GramToKg(Weight quantity);
+        Object TonneToKg(Weight quantity);
+        Object KgToTanne(Weight quantity);
+    }
+}
--- QuantityMeasurmentRepositoriesLayer/RepositoryImplementation/QuantityRepository.cs
+++ QuantityMeasurmentRepositoriesLayer/RepositoryImplementation/QuantityRepository.cs
@@ -1,10 +1,90 @@
-﻿using System;
+﻿using QuantityMeasurmentCommanLayer;
+using QuantityMeasurmentCommanLayer.Model;
+using QuantityMeasurmentRepositoriesLayer.Repository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace QuantityMeasurmentRepositoriesLayer.RepositoryImplementation
 {
-    class QuantityRepository
+    public class QuantityRepository :IQuantityRepository
     {
+        public object FeetToInch(Length quantity)
+        {
+            return quantity.Feet * 12;
+        }
+
+        public object FeetToYard(Length quantity)
+        {
+            return quantity.Yard /3;
+        }
+        public object CentimeterToInch(Length quantity)
+        {
+            return quantity.Centimeter / 2.54;
+        }
+        public object InchToCentimeter(Length quantity)
+        {
+            return quantity.Inch * 2.54;
+        }
+
+        public object InchToFeet(Length quantity)
+        {
+            return quantity.Inch / 12;
+        }
+
+        public object YardToFeet(Length quantity)
+        {
+            return quantity.Yard *3;
+        }
+
+        public object FahrenheitToCelsius(Temperature quantity)
+        {
+            return ((quantity.Fahrenheit - 32) *5 / 9 );
+        }
+
+        public object CelsiusToFahrenheit(Temperature quantity)
+        {
+            return ((quantity.Celsius * 9/5)+32);
+        }
+
+        public object MililiterToLiter(Volume quantity)
+        {
+            return quantity.Milliliter /1000;
+        }
+
+        public object LiterToMiliter(Volume quantity)
+        {
+            return quantity.Liter * 1000;
+        }
+
+        public object LiterToGallon(Volume quantity)
+        {
+            return quantity.Liter /3.785;
+        }
+
+        public object GallonToLiter(Volume quantity)
+        {
+            return quantity.Gallon * 3.785;
+        }
+
+        public object KgToGram(Weight quantity)
+        {
+            return quantity.Kilogram/1000;
+        }
+
+        public object GramToKg(Weight quantity)
+        {
+            return quantity.Gram * 1000;
+        }
+
+        public object TonneToKg(Weight quantity)
+        {
+            return quantity.Kilogram * 1000;
+        }
+
+        public object KgToTanne(Weight quantity)
+        {
+            return quantity.Tonne / 1000;
+        }
     }
 }
