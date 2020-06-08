53d2f763a4e06bef3255df1c264d756d4db8496f
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-06-08 09:52:42 +0530

    [Srinidhi]Added: Quantity measurment to calcluate all the quantitys

--- QuantityMeasurmentManagmentLayer/IManager/IQuantityManager.cs
+++ QuantityMeasurmentManagmentLayer/IManager/IQuantityManager.cs
@@ -4,30 +4,38 @@ using System;
 
 namespace QuantityMeasurmentManagmentLayer.IManager
 {
+    /// <summary>
+    /// Length(inch,feet,yard,centimeter)
+    /// Tempareture(fahreheit,celsius
+    /// Volume(mililiter,liter,gallon)
+    /// Weight(kg,gram,tonne)
+    /// </summary>
+    /// <param name="quantity"></param>
+    /// <returns></returns>
     public interface IQuantityManager
     {
         ////Length
-        Object FeetToInch(Length quantity);
-        Object InchToFeet(Length quantity);
-        Object FeetToYard(Length quantity);
-        Object YardToFeet(Length quantity);
-        Object InchToCentimeter(Length quantity);
-        Object CentimeterToInch(Length quantity);
+        object FeetToInch(Length quantity);
+        object InchToFeet(Length quantity);
+        object FeetToYard(Length quantity);
+        object YardToFeet(Length quantity);
+        object InchToCentimeter(Length quantity);
+        object CentimeterToInch(Length quantity);
 
         ////Tempareture
-        Object FahrenheitToCelsius(Temperature quantity);
-        Object CelsiusToFahrenheit(Temperature quantity);
+        object FahrenheitToCelsius(Temperature quantity);
+        object CelsiusToFahrenheit(Temperature quantity);
 
         ////Volume
-        Object MililiterToLiter(Volume quantity);
-        Object LiterToMiliter(Volume quantity);
-        Object LiterToGallon(Volume quantity);
-        Object GallonToLiter(Volume quantity);
+        object MililiterToLiter(Volume quantity);
+        object LiterToMiliter(Volume quantity);
+        object LiterToGallon(Volume quantity);
+        object GallonToLiter(Volume quantity);
 
         ////Weight
-        Object KgToGram(Weight quantity);
-        Object GramToKg(Weight quantity);
-        Object TonneToKg(Weight quantity);
-        Object KgToTanne(Weight quantity);
+        object KgToGram(Weight quantity);
+        object GramToKg(Weight quantity);
+        object TonneToKg(Weight quantity);
+        object KgToTanne(Weight quantity);
     }
 }
